0cabc54648d616f6309752cfaeae580bae99dadb
Kamali <kamzz1605@gmail.com@gmail.com>
2026-06-13 19:53:53 +0530

    fix: remove outputFileTracingRoot for Vercel compatibility

--- apps/crm/next.config.js
+++ apps/crm/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['pg'],
-  outputFileTracingRoot: path.join(__dirname, '../../'),
   optimizeFonts: false,
 };
 
